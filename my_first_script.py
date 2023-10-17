0ec7f470c60cdd3d4c11ceb5a62f64a5bc999699
pfb09 <pfb09@info09.cshl.edu>
2023-10-17 11:20:37 -0400

    sys.argv, one print statement

--- my_first_script.py
+++ my_first_script.py
@@ -2,8 +2,18 @@
 
 import sys
 
-# Store input into variable name
+# Store inputs into variable name
 name = sys.argv[1]
-print("My name: ", name)
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
 
+# print information out
+#print(f"My name: {name}")
+#print(f"My favorite color: {color} ")
+#print("My favorite activity: ", activity)
+#print("My favorite animal: ", animal)
+
+# printing all variables in one print statement
+print (f"My name: {name}\n"+f"My favorite color: {color}\n"+f"My favorite activity: {activity}\n"+f"My favorite animal: {animal}\n")
 
