cf13bb492d7acf37955f4b85c5ac57af076292a8
pfb09 <pfb09@info09.cshl.edu>
2023-10-17 11:24:41 -0400

    re-adding python 1 problem set script with corrected file name

---
+++ python_1_probset.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Store inputs into variable name
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+# print information out
+#print(f"My name: {name}")
+#print(f"My favorite color: {color} ")
+#print("My favorite activity: ", activity)
+#print("My favorite animal: ", animal)
+
+# printing all variables in one print statement
+print (f"My name: {name}\n"+f"My favorite color: {color}\n"+f"My favorite activity: {activity}\n"+f"My favorite animal: {animal}\n")
+
